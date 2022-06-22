3bb5164f890271814a31c3b8a516e137a6be7cc1
levraz1337 <evraz1337@ya.ru>
2022-06-22 14:16:37 +0300

    23

---
+++ p4ne/Lab2.1/Lab2.1.py
@@ -0,0 +1,33 @@
+#!/usr/local/bin/python3
+
+# Подключаем библиотечные функции
+from matplotlib import pyplot
+from openpyxl import load_workbook
+
+# Определяем служебную функцию выделения значения из ячейки
+def getvalue(x):
+    return x.value
+
+
+# Извлекаем таблицу MS Excel из файла в переменную
+wb = load_workbook('data_analysis_lab.xlsx')
+
+# Извлекаем лист из таблицы
+sheet = wb['Data']
+
+# Извлекаем и преобразуем три столбца из листа
+years = list(map(getvalue, sheet['A'][1:]))
+temperature = list(map(getvalue, sheet['C'][1:]))
+activity = list(map(getvalue, sheet['D'][1:]))
+
+# Создаём графическое представление, но пока не показываем
+pyplot.plot(years, temperature)
+pyplot.plot(years, activity)
+
+# Украшаем график - добавляем надписи
+pyplot.xlabel('Годы')
+pyplot.ylabel('Температура/Активность Солнца')
+pyplot.legend(loc='upper left')
+
+# Отображаем график
+pyplot.show()
